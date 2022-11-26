852f19d39f8d92dbca24db85d79cd2a367eebc2e
obinnafranklinduru <obinnafranklinduru.com>
2022-11-26 01:15:44 +0100

    initial commit

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,6 +1,20 @@
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
 
 async function main() {
+
+  // Address of the whitelist contract that you deployed in the previous module
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  // URL from where we can extract the metadata for a Crypto Dev NFT
+  const metadataURL = METADATA_URL;
+
+  const contract = await ethers.getContarctFactory("BinnaDevs");
+
+  const deployedContract = await contract.deploy(metadataURL, whitelistContract);
+
+  await deployedContract.deployed();
+
+  console.log(`BinnaDevs Contract Address => ${deployedContract}`);
   
 }
 
